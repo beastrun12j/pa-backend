5211fc1e39a299d15e80f3991168461fba3e530e
Pranav <pranavtiwari12j@gmail.com>
2023-01-04 18:23:45 +0530

    Revised commit

--- manage.py
+++ manage.py
@@ -17,20 +17,5 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = 'Creates a superuser.'
-
-    def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='pranavtiwari',
-                password='sourab@123'
-            )
-        print('Superuser has been created.')
-
 if __name__ == '__main__':
     main()
